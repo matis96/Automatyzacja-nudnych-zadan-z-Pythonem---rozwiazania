61cea652ef70fff403f5d7968042fdac6146c0e2
Matis <mateuszgrzyb96@gmail.com>
2024-09-29 19:31:16 +0200

    Create toss.py

---
+++ chapter 11/toss.py
@@ -0,0 +1,19 @@
+import random
+guess = ''
+while guess not in ('orzel', 'reszka'):
+    print('Odgadnij wynik rzutu monetą! Wpisz orzel lub reszka:')
+    guess = input()
+toss = random.randint(0, 1) #0 oznacza reszke, natomiast 1 to orzel.
+if toss == 0:
+    toss = 'reszka'
+else:
+    toss = 'orzel'
+if toss == guess:
+    print('Odgadles!')
+else:
+    print('Nie udalo sie! Sprobuj ponownie!')
+    guess = input()
+    if toss == guess:
+        print('Odgadles!')
+    else:
+        print('Nie udalo sie! Naprawde kiepsko Ci dzisi idzie.')
